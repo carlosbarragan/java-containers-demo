b19b3867980a5dbb5f96075d4bc7066474c3b0d7
Carlos Barragan <carlos.barragan@novatec-gmbh.de>
2022-11-16 18:06:21 +0100

    Added bash for running projects in docker

---
+++ micronaut/runInDocker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# before running this file, build the project first.
+cores=1
+if [ "$1" ];
+then
+  cores=$1
+fi
+set -x
+docker run -it --cpus "$cores" -m 2g -v ${PWD}:/app --workdir /app openjdk:19-jdk-alpine3.16 java -jar build/libs/cmndemo-0.1-all.jar
